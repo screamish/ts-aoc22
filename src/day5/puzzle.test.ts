d60f90da092190748fbb84550b29069f5971f322
Simon Fenton <swfenton@gmail.com>
2022-12-05 21:54:19 +1100

    day5 part1 solved

--- src/day5/puzzle.test.ts
+++ src/day5/puzzle.test.ts
@@ -3,30 +3,51 @@ import { go, go2 } from './puzzle'
 import { readFileSync } from 'fs'
 import path from 'path'
 
-let input = `
+let stack = `
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+`
 
+let moves = `
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
 `.trim()
 
-test('can pass the spec in the instructions for part 1', async t => {
-  let result = go(input)
+// test('can pass the spec in the instructions for part 1', async t => {
+//   let result = go(stack, moves)
 
-  t.is(result, 0)
+//   t.is(result, 'CMZ')
+// })
+
+let biggerStack = `
+                [V]     [C]     [M]
+[V]     [J]     [N]     [H]     [V]
+[G] [G] [F] [S] [D] [H] [B] [R] [S]
+`
+test('can parse another bigger sparse stack', async t => {
+  let result = go(biggerStack, 'move 1 from 1 to 2')
+
+  t.is(result, 'GVJSVHCRM')
 })
 
-const file = readFileSync(path.join(__dirname,'./data.txt'), 'utf-8').trim();
+const stacksFile = readFileSync(path.join(__dirname,'./stacks.txt'), 'utf-8')
+const movesFile = readFileSync(path.join(__dirname,'./moves.txt'), 'utf-8')
 
 test('can get answer for part 1', async t => {
-    let result = go(file)
-    t.is(result, 0)
+  let result = go(stacksFile, movesFile)
+  t.is(result, 'JRVNHHCSJ')
 })
 
 test('can pass the spec in the instructions for part 2', async t => {
-  let result = go2(input)
+  let result = go2(stack, moves)
 
-  t.is(result, 0)
+  t.is(result, '')
 })
 
 test('can get answer for part 2', async t => {
-    let result = go2(file)
-    t.is(result, 0)
+  let result = go2(stack, moves)
+  t.is(result, '')
 })
--- src/day5/puzzle.ts
+++ src/day5/puzzle.ts
@@ -1,9 +1,102 @@
-import { A, S, O, F, pipe, Option, N } from '@mobily/ts-belt'
+import { A, S, O, G, F, pipe, Option, N } from '@mobily/ts-belt'
+import { Stack, Range } from 'immutable'
 
-export const go = (intput:string) : number => {
-    return 0
+//     [D]    
+// [N] [C]    
+// [Z] [M] [P]
+const parseStack = (stack:string) : Stack<string>[] => {
+    const stacks = new Array<Stack<string>>()
+    pipe(
+        stack.split('\n'),
+        A.reverse,
+        A.forEach((line) => {
+            const l = pipe(
+                line.split(''),
+                A.splitEvery(4),
+                A.map(c => c[1] || ' '),
+                A.forEachWithIndex((i, c) : Stack<string> =>
+                    stacks[i] = c === ' '
+                        ? (stacks[i] || Stack())
+                        : (stacks[i] || Stack()).unshift(c)
+                )
+            )
+        })
+    )
+    return stacks
 }
 
-export const go2 = (intput:string) : number => {
-    return 0
+const printStacks = (stacks: Stack<string>[]) : void => {
+    A.forEachWithIndex(stacks, (i, s) => console.log(`stack ${i} is ${s}`))
+}
+
+type Move = {
+    quantity: number
+    from: number
+    to: number
+}
+
+// move 1 from 2 to 1
+// move 3 from 1 to 3
+// move 2 from 2 to 1
+// move 1 from 1 to 2
+const parseMoves = (movesInput:string) : Move[] => {
+    const moves = pipe(
+        movesInput.split('\n'),
+        A.map(line => {
+            const [, q, , f, , t] = line.split(' ')
+            return {
+                quantity: Number(q),
+                from: Number(f),
+                to: Number(t)
+            } as Move
+        }),
+        F.toMutable
+    )
+    return moves 
+}
+
+const performMovesOnStacks = (stacks: Stack<string>[], moves: Move[]) : Stack<string>[] => {
+    A.forEach(moves, move => {
+        const from = move.from - 1
+        const to = move.to - 1
+        pipe(
+            Range(1, move.quantity + 1).toArray(),
+            A.forEach(m => {
+                // console.log(`iter ${m} for move ${JSON.stringify(move)}`)
+                const crate = stacks[from]?.first() || '{BUG}'
+                stacks[from] = (stacks[from] || Stack()).shift()
+                stacks[to] = (stacks[to] || Stack()).unshift(crate)
+            })
+        )
+        // console.log(`after move ${JSON.stringify(move)}`)
+        // printStacks(stacks)
+    })
+    printStacks(stacks)
+    return stacks
+}
+
+const getTopCrate = (stacks: Stack<string>[]) : string => {
+    return pipe(
+        stacks,
+        A.map(s => s.first() || '_'), // _ for empty stack, helpful in debugging
+        A.join('')
+    )
+}
+
+export const go = (stackInput:string, movesInput:string) : string => {
+    const stacks = parseStack(stackInput)
+    const moves = parseMoves(movesInput)
+    // console.log('WHATTTTTTTTTTTTT')
+    // console.log(stacks)
+    printStacks(stacks)
+
+    // console.log(moves)
+
+    const result = performMovesOnStacks(stacks, moves)
+
+    return getTopCrate(result)
+}
+
+export const go2 = (stack:string, moves:string) : string => {
+    return ''
 }
