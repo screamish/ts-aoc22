2abb2de0f2e775347e3c9e79dcd293e142667e78
Simon Fenton <swfenton@gmail.com>
2022-12-08 00:42:45 +1100

    day7 part 1 solved - what a nightmare

--- src/day7/puzzle.test.ts
+++ src/day7/puzzle.test.ts
@@ -29,6 +29,21 @@ $ ls
 7214296 k
 `.trim()
 
+// let input = `
+// $ cd /
+// $ ls
+// dir a
+// 14848514 b.txt
+// 8504156 c.dat
+// dir d
+// $ cd a
+// $ ls
+// dir e
+// 29116 f
+// 2557 g
+// 62596 h.lst
+// `.trim()
+
 test('can pass the spec in the instructions for part 1', async t => {
   let result = go(input)
 
@@ -37,10 +52,10 @@ test('can pass the spec in the instructions for part 1', async t => {
 
 const file = readFileSync(path.join(__dirname,'./data.txt'), 'utf-8').trim();
 
-// test('can get answer for part 1', async t => {
-//     let result = go(file)
-//     t.is(result, 0)
-// })
+test('can get answer for part 1', async t => {
+    let result = go(file)
+    t.is(result, 0)
+})
 
 // test('can pass the spec in the instructions for part 2', async t => {
 //   let result = go2(input)
--- src/day7/puzzle.ts
+++ src/day7/puzzle.ts
@@ -1,4 +1,6 @@
-import { A, S, O, F, pipe, Option, N } from '@mobily/ts-belt'
+import { A, S, O, D, F, pipe, Option, N } from '@mobily/ts-belt'
+import { stringify } from 'querystring'
+import _ from 'lodash'
 
 // $ cd /
 // $ ls
@@ -24,80 +26,180 @@ import { A, S, O, F, pipe, Option, N } from '@mobily/ts-belt'
 // 5626152 d.ext
 // 7214296 k
 
-type SimpleFile = {
-    name: string
-    size: number
-}
+type MyDir = any // Record<string, SimpleFile | ?????????
 
-type MyDir = {
-    name: string
-    contents: (MyDir | SimpleFile)[]
-}
-
-type Command = cd | 'ls'
-type cd = { path: string }
-
-type ParseState =
-    'AwaitingOutputForLS' |
-    'Ready'
+type ParseState = 'AwaitingLs' | 'Ready'
 
 type Dirname = string
+type Filename = string
+type Filesize = number
 type LineParseResult =
-    SimpleFile |
-    Dirname |
-    Command
+    { kind: 'file', name: Filename, size: Filesize } |
+    { kind: 'dir', dir: Dirname } |
+    { kind: 'ls' } |
+    { kind: 'cd', path: string }
 
 const parseLine = (i: string) : LineParseResult => {
     const cdPattern = /\$ cd ([\w\.\/]+)/
     if (cdPattern.test(i)) {
         const [, path] = cdPattern.exec(i) || []
-        return { path: path || 'cd missing path' } as cd
+        return { kind: 'cd', path: path || 'cd missing path' } as LineParseResult
     }
     const lsPattern = /\$ ls/
     if (lsPattern.test(i)) {
-        return 'ls'
+        return { kind: 'ls' } as LineParseResult
     }
     const filePattern = /(\d+)\s([\w\.]+)/
     if (filePattern.test(i)) {
         const [, size, name] = filePattern.exec(i) || []
-        return { name: name || 'file missing name', size: size || -1 } as SimpleFile
+        // avoid collision with the lodash path syntax - ugh, poor choices
+        const sanitisedName = (name || 'file missing name').replace('.', '-')
+        return { kind: 'file', name: sanitisedName , size: Number(size) || -1 } as LineParseResult
     }
     const dirPattern = /dir (\w+)/
     if (dirPattern.test(i)) {
         const [, path] = dirPattern.exec(i) || []
-        return path || 'dir missing path' as Dirname
+        return { kind: 'dir', dir: path || 'dir missing path' as Dirname } as LineParseResult
     }
     throw new Error(`parse error on line: ${i}`);
 }
 
+const newDir = () : MyDir => ({ 'root': {} })
+
+type Cwd = { path: string[] }
+type Acc = {
+    state: ParseState
+    cwd: Cwd
+    dir: MyDir
+}
+const executeLine = (acc: Acc, line: LineParseResult) : Acc => {
+    const {state, cwd, dir} = acc
+    // console.log(`acc: ${JSON.stringify(acc)}, line: ${JSON.stringify(line)}`)
+    switch (state) {
+        case 'Ready':
+            // console.log(`PROCESSING LINE: ${JSON.stringify(line)}`)
+            if(line.kind === 'cd') {
+                    // console.log(`EXEC: cd with ${JSON.stringify(acc)}`)
+                    const path = line.path
+                    if (path === '..') {
+                        cwd.path.pop()
+                    } else if (path === '/') {
+                        cwd.path = ['root']
+                    } else {
+                        cwd.path.push(path)
+                    }
+                    return acc
+            }
+            if(line.kind === 'file') {
+                    const filePath = cwd.path.join('.') + '.' + line.name
+                    // console.log(`FILE: path: ${filePath}, name:${line.name}`)
+                    _.set(dir, filePath, line.size)
+                    // console.log(`NEWDIR: ${JSON.stringify(dir)}`)
+                    return acc
+            }
+            switch (line.kind) {
+                // case 'ls':
+                //     console.log(`exec: ls with ${JSON.stringify(acc)}`)
+                //     return { ...acc, state: 'AwaitingLs' }
+                // case 'cd':
+                //     console.log(`exec: cd with ${JSON.stringify(acc)}`)
+                //     const path = line.path
+                //     if (path === '..') {
+                //         cwd.path.pop()
+                //     } else if (path === '/') {
+                //         cwd.path = []
+                //     } else {
+                //         cwd.path.push(path)
+                //     }
+                //     return { state: 'Ready', dir, cwd }
+                //     // console.log()
+                // case 'file':
+                    // const filePath = cwd.path.join('.')
+                    // const dir1 = _.update(dir, filePath, d => _.set(d, line.name, ({ size: line.size} as SimpleFile)))
+                    // return { state, dir: dir1, cwd }
+                default:
+                    // console.log(`IGNORING: ${JSON.stringify(line)}`)
+                    // console.error(`Unexpected input in Ready state, got: ${JSON.stringify(line)}`)
+                    break
+                }
+            break
+        case 'AwaitingLs':
+            switch (line.kind) {
+                // case 'file':
+                //     const filePath = cwd.path.join('.')
+                //     const dir1 = _.update(dir, filePath, d => _.set(d, line.name, ({ size: line.size} as SimpleFile)))
+                //     return { state, dir: dir1, cwd }
+                // case 'dir':
+                //     console.log(`IGNORING: dir entry for ${line.dir}`)
+                //     break
+                default:
+                    console.error(`Unexpected input in AwaitingLs state, got: ${JSON.stringify(line)}`)
+                    break
+            }
+            break
+    
+        default:
+            console.error(`Unexpected state, got: ${JSON.stringify(acc)}`)
+            break
+    }
+    return { state, dir, cwd }
+}
+
 const parse = (input: string) : MyDir => {
+    const initialState = {
+        state: 'Ready',
+        cwd: { path: ['root'], contents: newDir() },
+        dir: newDir()
+    } as Acc
     const x = pipe(
         input.split('\n'),
-        A.reduce({ state: 'Ready', dir: { name: 'root', contents: [] } as MyDir }, 
-            ({ state, dir }, line) => {
-                const lineResult = parseLine(line)
-                console.log(`line: ${line} | ${JSON.stringify(lineResult)}`)
-                return { state, dir }
-                // switch (state) {
-                //     case 'Ready':
-                        
-                //         break
-                //     case 'AwaitingOutputForLS':
-
-                //         break
-                
-                //     default:
-                //         break
-                // }
-            }),
-        (x => x.dir )
+        A.map(parseLine),
+        // console.log(`line: ${line} | ${JSON.stringify(lineResult)} | state: ${state}`)
+        A.reduce(initialState, executeLine),
+        (x => x.dir)
     )
     return x
 }
 
+type WalkResult = { size: number, subdirs: Record<string, number> }
+const walkDir = (dir: any): WalkResult  => {
+    const zero: WalkResult = { size: 0, subdirs: D.fromPairs([]) }
+    const d = pipe(
+        D.toPairs(dir),
+        A.reduce(zero, (({ size, subdirs }, [name, entry]) => {
+            // console.log(`walk iter name:${name} & entry:${entry}`)
+            if (typeof(entry) === 'number') {
+                return { size: size + entry, subdirs }
+            }
+            if (typeof(entry) === 'object') {
+                const r = walkDir(entry)
+                const obfuscatedName = crypto.randomUUID()
+                const newsubdirs = pipe(
+                    subdirs,
+                    D.set(obfuscatedName, r.size),
+                    D.merge(r.subdirs)
+                )
+                // console.log(`NEW SUBDIRS: ${JSON.stringify(newsubdirs)}`)
+                return { size: size + r.size, subdirs: newsubdirs }
+            }
+            throw new Error(`unexpected entry in walkDir: ${JSON.stringify(entry)}`)
+        }))
+    )
+    return d
+}
+
 export const go = (input:string) : number => {
     const dir = parse(input)
-    return 0
+    console.log(`DIR: ${JSON.stringify(dir, null, 2)}`)
+    const sizes = walkDir(dir)
+    console.log(`FLATTENED DIR SIZES: ${JSON.stringify(sizes, null, 2)}`)
+    const result = pipe(
+        sizes.subdirs,
+        D.values,
+        A.filter(i => i <= 100000)
+    )
+    console.log(`FILTERED DIR SIZES: ${JSON.stringify(result, null, 2)}`)
+    return _.sum(result)
 }
 
 export const go2 = (input:string) : number => {
